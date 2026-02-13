3eabc6b0abe6c2cb5788ca9e78c685789c80108e
Lucas Menargues <kluklassmr@gmail.com>
2026-02-13 01:50:21 +0100

    WIP: Register

---
+++ composeApp/src/commonMain/kotlin/ies/sequeros/dam/pmdm/gestionperifl/ui/home/HomeScreen.kt
@@ -0,0 +1,16 @@
+package ies.sequeros.dam.pmdm.gestionperifl.ui.home
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+
+@Composable
+
+fun HomeScreen(
+
+) {
+    Box(
+
+    ) {
+
+    }
+}
