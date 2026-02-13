9fc91fc9d9ed882063801f30ace31612d9aaa928
Lucas Menargues <kluklassmr@gmail.com>
2026-02-13 15:36:36 +0100

    WIP: Register

--- composeApp/src/commonMain/kotlin/ies/sequeros/dam/pmdm/gestionperifl/aplicacion/register/RegisterUseCase.kt
+++ composeApp/src/commonMain/kotlin/ies/sequeros/dam/pmdm/gestionperifl/aplicacion/register/RegisterUseCase.kt
@@ -1,10 +1,11 @@
 package ies.sequeros.dam.pmdm.gestionperifl.aplicacion.register
 
 import ies.sequeros.dam.pmdm.gestionperifl.dominio.IUserRepository
+import ies.sequeros.dam.pmdm.gestionperifl.infraestructure.entities.RegisterDto
 
 
 class RegisterUseCase(private val repository: IUserRepository) {
-    suspend operator fun invoke(registerCommand: RegisterCommand): Result<Boolean> {
+    suspend operator fun invoke(registerCommand: RegisterCommand): Result<RegisterDto> {
         return repository.register(registerCommand)
     }
 }
